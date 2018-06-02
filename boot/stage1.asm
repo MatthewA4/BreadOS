502c272e3e06d449680d009aa98608e5619fd5e0
Matthew Anderson <mattanderson65@outlook.com>
2018-06-02 15:21:36 -0700

    Clarify licensing of the files.

--- boot/stage1.asm
+++ boot/stage1.asm
@@ -1,5 +1,5 @@
 ; Copyright (C) 2018 Matthew Anderson. mattanderson65@outlook.com
-;See LICENSE file
+;This code is released under the MIT License, see LICENSE file.
 
 ; inspired by the bootloader and operating system tutorials at
 ; http://brokenthorn.com . I thank the author so much if he ever reads this.
